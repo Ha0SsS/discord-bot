9a9420a10cb5f76a1757dc9c8fa99f2bd641effd
Haos Harbour <70344955+Ha0SsS@users.noreply.github.com>
2022-11-25 00:39:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  OWNER_IDS: [530664668532834304], // Bot owner ID's
+  OWNER_IDS: [], // Bot owner ID's
   SUPPORT_SERVER: "", // Your bot support server
   PREFIX_COMMANDS: {
     ENABLED: true, // Enable/Disable prefix commands
@@ -9,7 +9,7 @@ module.exports = {
     SLASH: true, // Should the interactions be enabled
     CONTEXT: true, // Should contexts be enabled
     GLOBAL: true, // Should the interactions be registered globally
-    TEST_GUILD_ID: "861365125298126858", // Guild ID where the interactions should be registered. [** Test you commands here first **]
+    TEST_GUILD_ID: "uuuuuuu", // Guild ID where the interactions should be registered. [** Test you commands here first **]
   },
   EMBED_COLORS: {
     BOT_EMBED: "#068ADD",
